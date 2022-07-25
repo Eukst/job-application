bdc7f3b33e74824705ff8ecf7974f4b074318980
ErManoj-Sharma <manoj.sharma@ongraphmail.com>
2022-07-25 11:01:29 +0530

    PR Issue fixed single  validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@
 
 # User class
 class User < ApplicationRecord
-  has_many:jobs , dependent: :destroy
+  has_many :jobs, dependent: :destroy
   # Include default devise modules. Others available are:
   # , :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
@@ -11,12 +11,6 @@ class User < ApplicationRecord
   # validation
   VALID_PIN_REGEX = /[1-9]{1}[0-9]{5}/
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
-  validates :address, presence: true
-  validates :mobile_no, presence: true
-  validates :city, presence: true
-  validates :state, presence: true
-  validates :user_role, presence: true
-  validates :pincode, presence: true,format: { with: VALID_PIN_REGEX },length: {is: 6}
+  validates :first_name, :last_name, :address, :mobile_no, :city, :state, :user_role, presence: true
+  validates :pincode, presence: true, format: { with: VALID_PIN_REGEX }, length: { is: 6 }
 end
