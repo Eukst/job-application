50eb704d733209aaa137a29f8fc0a10986018a99
ErManoj-Sharma <manoj.sharma@ongraphmail.com>
2022-07-26 10:45:17 +0530

    Added Unique validation in Applyjob model

--- app/models/apply_job.rb
+++ app/models/apply_job.rb
@@ -1,4 +1,9 @@
+# frozen_string_literal:true
+
+# class for Association M2M
 class ApplyJob < ApplicationRecord
-    belongs_to:user
-    belongs_to:job
+  belongs_to :user
+  belongs_to :job
+  validates :user_id, presence: true, uniqueness: true
+  validates :job_id, presence: true, uniqueness: true
 end
