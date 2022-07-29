2e51c03f7a739fd56b35a5e69c51cb195dfcf6ea
ErManoj-Sharma <manoj.sharma@ongraphmail.com>
2022-07-29 08:43:52 +0530

    model validation test cases added

--- test/models/apply_job_test.rb
+++ test/models/apply_job_test.rb
@@ -1,7 +1,25 @@
-require "test_helper"
+# frozen_string_literal:true
+
+require 'test_helper'
 
 class ApplyJobTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @apply_job1 = apply_jobs(:one)
+    @apply_job2 = apply_jobs(:two)
+    @apply_job3 = apply_jobs(:three)
+  end
+  test 'apply job 1 should be valid ' do
+    assert @apply_job1.valid?
+  end
+  test 'apply job 2 should be valid ' do
+    assert @apply_job2.valid?
+  end
+  test 'apply job 3 should be valid ' do
+    assert @apply_job3.valid?
+  end
+  test 'user_id job_id pair should be unique' do
+    duplicate_pair = @apply_job1.dup
+    @apply_job1.save
+    assert_not duplicate_pair.valid?
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,80 @@
-require "test_helper"
+require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = users(:user1)
+  end
+
+  test 'user should be valid ' do
+    assert @user.valid?
+  end
+
+  test 'first name should be present' do
+    @user.first_name = '    '
+    assert_not @user.valid?
+  end
+  test 'last name should be present' do
+    @user.last_name = '    '
+    assert_not @user.valid?
+  end
+  test 'email should be present' do
+    @user.email = '    '
+    assert_not @user.valid?
+  end
+  test 'address should be present' do
+    @user.address = '    '
+    assert_not @user.valid?
+  end
+  test 'mobile_no should be present' do
+    @user.mobile_no = '    '
+    assert_not @user.valid?
+  end
+  test 'city should be present' do
+    @user.city = '    '
+    assert_not @user.valid?
+  end
+  test 'state should be present' do
+    @user.state = '    '
+    assert_not @user.valid?
+  end
+  test 'user role should be present' do
+    @user.user_role = '    '
+    assert_not @user.valid?
+  end
+  # length validations
+
+  test 'pin should have 6 digit only ' do
+    @user.pincode = '9' * 6
+    assert @user.valid?
+  end
+  test 'pin should have not more than 6 ' do
+    @user.pincode = '9' * 7
+    assert_not @user.valid?
+  end
+  test 'pin should have not less than 6 ' do
+    @user.pincode = '9' * 5
+    assert_not @user.valid?
+  end
+  test 'pincode first digit not be zero ' do
+    @user.pincode = '0' + '9' * 5
+    assert_not @user.valid?
+  end
+
+  test 'mobile number should be less than 15' do
+    @user.mobile_no = '9' * 16
+    assert_not @user.valid?
+  end
+  test 'mobile number should be grateher than 9' do
+    @user.mobile_no = '9' * 9
+    assert_not @user.valid?
+  end
+  test 'mobile number with 15 digit should pass' do
+    @user.mobile_no = 999999999999999
+    assert @user.valid?
+  end
+  test 'mobile number with 10 digit should pass' do
+    @user.mobile_no = 9999999999
+    assert @user.valid?
+  end
+  
 end
